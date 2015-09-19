bb4dfdb9993e5c784f862d43699007192b6d5a67
Donald Fung <donaldtfung@gmail.com>
2015-09-19 07:07:24 -0400

    unedited xcode project added

---
+++ projectSnowball/projectSnowball/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  projectSnowball
+//
+//  Created by Donald Fung on 9/19/15.
+//  Copyright (c) 2015 Donald Fung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ projectSnowball/projectSnowball/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  projectSnowball
+//
+//  Created by Donald Fung on 9/19/15.
+//  Copyright (c) 2015 Donald Fung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
