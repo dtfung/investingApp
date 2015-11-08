4519444923abe338b7b18cb78e50404913cfc659
Donald Fung <donaldtfung@gmail.com>
2015-11-08 08:03:43 -0500

    Added Company Detail view controller
    handled segue to new view controller once the user submits search
    input.

---
+++ projectSnowball/projectSnowball/CompanyDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  CompanyDetailViewController.h
+//  projectSnowball
+//
+//  Created by Donald Fung on 11/7/15.
+//  Copyright © 2015 Donald Fung & Blayne Cameron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CompanyDetailViewController : UIViewController
+
+@end
